120ea2f4a5e1babacfdb7b46eeab28d74d218250
Salim <slmbouskine93@gmail.com>
2025-11-29 02:35:19 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Salim on 29/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
